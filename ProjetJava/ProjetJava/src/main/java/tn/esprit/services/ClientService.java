b34d32d62966556865ee637bf2663bdd26e76686
Sara30Othmani <147887362+Sara30Othmani@users.noreply.github.com>
2024-03-16 02:31:58 +0100

    Update ClientService.java

--- ProjetJava/ProjetJava/src/main/java/tn/esprit/services/ClientService.java
+++ ProjetJava/ProjetJava/src/main/java/tn/esprit/services/ClientService.java
@@ -1,1 +1,28 @@
+public class ClientService {
+    private ClientDao clientDao = new ClientDao();
 
+    public void ajouterCollaboration(int idClient, String collaboration) {
+        Client client = clientDao.getClient(idClient);
+        if (client != null) {
+            client.ajouterCollaboration(collaboration);
+            clientDao.updateClient(client);
+        }
+    }
+
+    public void supprimerCollaboration(int idClient, String collaboration) {
+        Client client = clientDao.getClient(idClient);
+        if (client != null) {
+            client.supprimerCollaboration(collaboration);
+            clientDao.updateClient(client);
+        }
+    }
+
+    public void modifierCollaboration(int idClient, String ancienneCollaboration, String nouvelleCollaboration) {
+        Client client = clientDao.getClient(idClient);
+        if (client != null) {
+            client.modifierCollaboration(ancienneCollaboration, nouvelleCollaboration);
+            clientDao.updateClient(client);
+        }
+    }
+
+}
