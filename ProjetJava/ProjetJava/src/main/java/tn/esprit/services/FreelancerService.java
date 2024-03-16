4663ad18864c6bb35823c67869e2ee0d21d26ada
Sara30Othmani <147887362+Sara30Othmani@users.noreply.github.com>
2024-03-16 02:14:32 +0100

    Update FreelancerService.java

--- ProjetJava/ProjetJava/src/main/java/tn/esprit/services/FreelancerService.java
+++ ProjetJava/ProjetJava/src/main/java/tn/esprit/services/FreelancerService.java
@@ -1,1 +1,45 @@
+public class FreelancerService {
+    private FreelancerDao freelancerDao = new FreelancerDao();
+
+    public void ajouterCompetence(int idFreelancer, String competence) {
+        Freelancer freelancer = freelancerDao.getFreelancer(idFreelancer);
+        if (freelancer != null) {
+            freelancer.ajouterCompetence(competence);
+            freelancerDao.updateFreelancer(freelancer);
+        }
+    }
+
+    public void supprimerCompetence(int idFreelancer, String competence) {
+        Freelancer freelancer = freelancerDao.getFreelancer(idFreelancer);
+        if (freelancer != null) {
+            freelancer.supprimerCompetence(competence);
+            freelancerDao.updateFreelancer(freelancer);
+        }
+    }
+
+    public void modifierCompetence(int idFreelancer, String ancienneCompetence, String nouvelleCompetence) {
+        Freelancer freelancer = freelancerDao.getFreelancer(idFreelancer);
+        if (freelancer != null) {
+            freelancer.modifierCompetence(ancienneCompetence, nouvelleCompetence);
+            freelancerDao.updateFreelancer(freelancer);
+        }
+    }
+
+    public void ajouterCertification(int idFreelancer, String certification) {
+        Freelancer freelancer = freelancerDao.getFreelancer(idFreelancer);
+        if (freelancer != null) {
+            freelancer.ajouterCertification(certification);
+            freelancerDao.updateFreelancer(freelancer);
+        }
+    }
+
+    public void supprimerCertification(int idFreelancer, String certification) {
+        Freelancer freelancer = freelancerDao.getFreelancer(idFreelancer);
+        if (freelancer != null) {
+            freelancer.supprimerCertification(certification);
+            freelancerDao.updateFreelancer(freelancer);
+        }
+    }
+
+}
 
