0b36e3ab262d2dcace7e77e319b43f3f4fa5481d
Sara30Othmani <147887362+Sara30Othmani@users.noreply.github.com>
2024-03-16 02:31:15 +0100

    Update ClientController.java

--- ProjetJava/ProjetJava/src/main/java/tn/esprit/controllers/ClientController.java
+++ ProjetJava/ProjetJava/src/main/java/tn/esprit/controllers/ClientController.java
@@ -1,1 +1,16 @@
+public class ClientController {
+    private ClientService clientService = new ClientService();
 
+    public void ajouterCollaboration(int idClient, String collaboration) {
+        clientService.ajouterCollaboration(idClient, collaboration);
+    }
+
+    public void supprimerCollaboration(int idClient, String collaboration) {
+        clientService.supprimerCollaboration(idClient, collaboration);
+    }
+
+    public void modifierCollaboration(int idClient, String ancienneCollaboration, String nouvelleCollaboration) {
+        clientService.modifierCollaboration(idClient, ancienneCollaboration, nouvelleCollaboration);
+    }
+
+}
