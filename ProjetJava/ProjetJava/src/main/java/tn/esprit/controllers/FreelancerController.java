fcc845ca71abd803e1301e9cf7d97535485a5b03
Sara30Othmani <147887362+Sara30Othmani@users.noreply.github.com>
2024-03-16 02:08:16 +0100

    Update FreelancerController.java

--- ProjetJava/ProjetJava/src/main/java/tn/esprit/controllers/FreelancerController.java
+++ ProjetJava/ProjetJava/src/main/java/tn/esprit/controllers/FreelancerController.java
@@ -1,1 +1,25 @@
+public class FreelancerController {
+    private FreelancerService freelancerService = new FreelancerService();
+
+    public void ajouterCompetence(int idFreelancer, String competence) {
+        freelancerService.ajouterCompetence(idFreelancer, competence);
+    }
+
+    public void supprimerCompetence(int idFreelancer, String competence) {
+        freelancerService.supprimerCompetence(idFreelancer, competence);
+    }
+
+    public void modifierCompetence(int idFreelancer, String ancienneCompetence, String nouvelleCompetence) {
+        freelancerService.modifierCompetence(idFreelancer, ancienneCompetence, nouvelleCompetence);
+    }
+
+    public void ajouterCertification(int idFreelancer, String certification) {
+        freelancerService.ajouterCertification(idFreelancer, certification);
+    }
+
+    public void supprimerCertification(int idFreelancer, String certification) {
+        freelancerService.supprimerCertification(idFreelancer, certification);
+    }
+
+}
 
