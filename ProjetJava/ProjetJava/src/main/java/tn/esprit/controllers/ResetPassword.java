433f3525b91480d256701b89e5125dba70195a22
Sara30Othmani <sara.othmani@esprit.tn>
2024-03-30 16:26:48 +0100

    Initiation à l'interface graphique

---
+++ ProjetJava/ProjetJava/src/main/java/tn/esprit/controllers/ResetPassword.java
@@ -0,0 +1,14 @@
+package tn.esprit.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class ResetPassword {
+    @FXML
+    private TextField TFMailReset;
+
+    public void Reset(ActionEvent event) {
+    }
+}
---
+++ ProjetJava/ProjetJava/src/main/java/tn/esprit/controllers/SignUp.java
@@ -0,0 +1,28 @@
+package tn.esprit.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUp {
+
+    @FXML
+    private DatePicker TFBirthDate;
+
+    @FXML
+    private TextField TFEmailAddress;
+
+    @FXML
+    private TextField TFName;
+
+    @FXML
+    private PasswordField TFPasswordCreated;
+
+    @FXML
+    void SignUp(ActionEvent event) {
+
+    }
+
+}
